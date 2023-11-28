3e17e2bef6dd9e5fadacc82ea1435191280379fc
vankur017 <128570689+vankur017@users.noreply.github.com>
2023-11-28 18:18:38 +0000

    conversion

---
+++ 01_basics/_conversionoperation.js
@@ -0,0 +1,21 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+//console.log(valueInNumber)
+
+//33 => 33
+//33abc => NaN 
+//true => 1
+//False =>0
+//null =>0
+
+let booleanIsLoggedin = "dsad01";
+
+let isloggedIn = Boolean(booleanIsLoggedin);
+console.log(isloggedIn)
+//0 =>True , 1 => False
+//"" => False, "anystring" =>true
