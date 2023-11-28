b836dfb08b0fa3ccb196e0a3b0dc237fa852c97c
vankur017 <128570689+vankur017@users.noreply.github.com>
2023-11-28 18:53:05 +0000

    Operations

--- 01_basics/_conversionoperation.js
+++ 01_basics/_conversionoperation.js
@@ -1,21 +1,47 @@
-let score = null
+// let score = null
 
-//console.log(typeof score);
-//console.log(typeof (score));
+// //console.log(typeof score);
+// //console.log(typeof (score));
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-//console.log(valueInNumber)
+// //console.log(valueInNumber)
 
-//33 => 33
-//33abc => NaN 
-//true => 1
-//False =>0
-//null =>0
+// //33 => 33
+// //33abc => NaN 
+// //true => 1
+// //False =>0
+// //null =>0
 
-let booleanIsLoggedin = "dsad01";
+// let booleanIsLoggedin = "dsad01";
 
-let isloggedIn = Boolean(booleanIsLoggedin);
-console.log(isloggedIn)
-//0 =>True , 1 => False
-//"" => False, "anystring" =>true
+// let isloggedIn = Boolean(booleanIsLoggedin);
+// //console.log(isloggedIn)
+// //0 =>True , 1 => False
+// //"" => False, "anystring" =>tr
+
+// let someValue = 33
+// let stringNumber = String(someValue)
+
+// console.log(typeof stringNumber)
+
+//*********************** OPERATIONS ****************************** *
+
+let str1 = "ankur";
+let str2 = " verma";
+
+//console.log(str1 + str2)
+//let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log(1 + "2")
+// console.log(1 + 2 + "2") =>32
+// console.log("2" + 1 + 2)=>212
+// console.log(+true)
+// console.log(+"")
+
+let x =3;
+
+
+const z = ++x;
+console.log(`z:${z}, x:${x}`)
