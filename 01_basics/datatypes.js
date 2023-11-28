223d655954f3473a752d628e99da1cef923244c2
vankur017 <128570689+vankur017@users.noreply.github.com>
2023-11-28 17:45:58 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+//number 2 to power 53
+//BigInt
+//string => ""
+//boolean => True or False
+//null => standalone value 
+//undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
