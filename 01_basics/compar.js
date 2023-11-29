c237e5a06716f142eea767da9c8383f9d5b7e082
vankur017 <128570689+vankur017@users.noreply.github.com>
2023-11-29 18:28:09 +0000

    compartors

---
+++ 01_basics/compar.js
@@ -0,0 +1,14 @@
+// // //console.log("2">0);
+// // console.log("2">0);
+
+// console.log(null >0);
+// console.log(null >=0);
+// console.log(null ==0);
+
+// console.log(undefined >0); false
+// console.log(undefined ==0); false
+// console.log(undefined <0); false
+
+//  === strict check
+
+//console.log("2" === 2); also checks if both datatypes are same or not
